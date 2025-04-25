eafb14594633b37fb34eb0f0675f07696a44c624
themonstersd13 <saurabhdoiphode1335@gmail.com>
2025-04-26 00:47:46 +0530

    done with WDDL

--- exp 7/server.js
+++ exp 7/server.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3500
 
-// Existing routes
 app.get('/', (req, res) => {
   res.send('Welcome to the Home Page')
 })
@@ -15,19 +14,16 @@ app.get('/contact', (req, res) => {
   res.send('Contact us at: email@example.com')
 })
 
-// New route: dynamic user ID
 app.get('/users/:id', (req, res) => {
   const { id } = req.params
   res.send(`User ID: ${id}`)
 })
 
-// New route: dynamic product category and ID, responds with JSON
 app.get('/products/:category/:productId', (req, res) => {
   const { category, productId } = req.params
   res.json({ category, productId })
 })
 
-// 404 handler for any other routes
 app.use((req, res) => {
   res.status(404).send('Page Not Found')
 })
